7e57ab1c498bc3c4a368428eb320be98fa3b3f25
MrMalcav <jakyeworking@gmail.com>
2018-11-04 18:43:56 +0100

    Creadas las dependencias y un Hola mundo

---
+++ bakendninja/src/main/java/com/udemy/BakendninjaaApplication.java
@@ -0,0 +1,12 @@
+package com.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendninjaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendninjaaApplication.class, args);
+	}
+}
