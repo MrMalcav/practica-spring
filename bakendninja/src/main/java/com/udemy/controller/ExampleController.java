10067dbcee692477e3ba5c8ee71de463185093e9
MrMalcav <jakyeworking@gmail.com>
2018-11-25 23:26:49 +0100

    Añadido ejemplo con listas

--- bakendninja/src/main/java/com/udemy/controller/ExampleController.java
+++ bakendninja/src/main/java/com/udemy/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.udemy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +21,7 @@ public class ExampleController {
 	@GetMapping("/exampleString")
 	public String exampleString(Model model)
 	{
-		model.addAttribute("person", new Person("Jon",23));
+		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
 	
@@ -27,8 +30,17 @@ public class ExampleController {
 	public ModelAndView exampleMAV()
 	{
 		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("person", new Person("Michael",30));
+		mav.addObject("people", getPeople());
 		return mav;
+	}
+	
+	private List <Person> getPeople(){
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Jon", 23));
+		people.add(new Person("Mikel", 30));
+		people.add(new Person("Eva", 43));
+		people.add(new Person("Peter", 18));
+		return people;
 	}
 
 }
